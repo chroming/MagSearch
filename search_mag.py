d65719eb8efbbd65c45455f1ba0c43fce3d07a5c
chroming <guangzhishang@gmail.com>
2017-03-31 23:54:12 +0800

    添加alicili.org的请求类

--- search_mag.py
+++ search_mag.py
@@ -5,10 +5,14 @@ from crawl_info import *
 
 
 class MagRequests(object):
-
-    def __init__(self, keyword):
+    """
+    请求基类
+    """
+    def __init__(self):
         self.url = ''
-        self.keyword = keyword
+        self.keyword = ''
+        self.re_exp = ''
+        self.xpath_exp = ''
         self.header = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:43.0) Gecko/20100101 Firefox/43.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -17,19 +21,71 @@ class MagRequests(object):
         'DNT': '1'
     }
 
-    @property
-    def postdata(self):
-        return self.__make_postdata__()
+    def search_keyword(self, keyword):
+        self.keyword = keyword
 
     def __make_postdata__(self):
         pass
 
-    def web_get(self):
-        return requests.get(self.url, headers=self.header).text
+    @property
+    def postdata(self):
+        return self.__make_postdata__()
 
-    def web_post(self):
-        return requests.post(self.url, headers=self.header, data=self.postdata).text
+    def web_get(self, url=None):
+        return requests.get(url if url else self.url, headers=self.header).text
 
-    @staticmethod
-    def get_result(result):
+    def web_post(self, url=None):
+        return requests.post(url if url else self.url, headers=self.header, data=self.postdata).text
+
+    def get_result(self):
         pass
+
+
+class AliRequest(MagRequests):
+    """
+    http://alicili.org/ 请求类
+    """
+    def __init__(self):
+        super(AliRequest, self).__init__()
+
+    def search_keyword(self, keyword):
+        self.keyword = keyword
+        self.url = 'http://alicili.org/list/%s/1-0-0/' % keyword
+
+    def __make_postdata__(self):
+        return {'keyword': self.keyword}
+
+    def get_result(self):
+        result = self.web_post()
+        return re_search(result, r'%s' % 'http://alicili.org/cili/\S*/')
+
+    def show_result(self):
+        return self.get_result()
+
+    def get_mag_result(self, url):
+        result = self.web_get(url)
+        # return re_search(result, r'%s' % 'magnet:\?xt=urn:btih:\S{40}')
+        return re_search(result,
+                         u'种子哈希：<b>(\w{40})</b><br />\s+文件数目：<b>(\d+)</b>个文件 <br />\s+'
+                         u'文件大小：<b>.{1,20}</b>或<b>([\d,]+) Bytes</b><br />\s+收录时间：<b>(.*?)</b><br />\s+'
+                         u'已经下载：<b>(\d+)</b>次<br />\s+下载速度：<b>.*?</b><br />\s+最近下载：<b>.*?</b></p>')
+        # return re_search(result, u'种子哈希：<b>(.*?)<b>')
+
+    def show_mag_result(self):
+        """
+        [[(u'bacac95472354ae0000e4f127f85b21a20897302', u'11', u'1,673,258,869', u'2016-08-19 22:30:19', u'17604')],……]
+        """
+        url_list = self.show_result()
+        mag_list = []
+        for url in url_list:
+            mag_list.append(self.get_mag_result(url))
+        return mag_list
+
+if __name__ == '__main__':
+    a = AliRequest()
+    a.search_keyword('soe-917')
+    a.show_mag_result()
+
+
+
+
