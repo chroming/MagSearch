2d509f409b0e72225553a17bb544dac48cef1205
chroming <guangzhishang@gmail.com>
2017-04-06 22:31:31 +0800

    过滤增加处理单条结果的选项

--- result_filter.py
+++ result_filter.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from tools import log_wrap
+from tools import *
 
 
 class ResultFilter(object):
@@ -7,40 +7,46 @@ class ResultFilter(object):
     filter_dict: {'contain':unicode, 'size':[min,max]}
     """
     @log_wrap
-    def __call__(self, result_list, filter_dict):
+    def __call__(self, result, filter_dict, result_type='list'):
         """
         
-        :param result_list: 需要处理的结果列表
+        :param result: 需要处理的结果, 可能为结果列表或单项结果
         :param filter_dict: 过滤规则, {'contain':unicode, 'size':[min,max]} 
         :return: 过滤结果
         """
-        self.result_list = result_list
+        self.result_list = result
         self.contain_uni = filter_dict['contain']
         self.size_min, self.size_max = filter_dict['size']
+        return self.__all_or_one__(result_type)
+
+    def __all_or_one__(self, result_type):
+        return self.receive_all_result() if result_type == 'list' else self.receive_one_result()
+
+    def receive_all_result(self):
         self._contains_()
         self._size_limits_()
         return self.result_list
+
+    def receive_one_result(self):
+        return (self._contain_(self.result_list) if self.contain_uni else True) & \
+               (self._size_limit_(self.result_list) if self.size_min or self.size_max else True)
 
     def _contains_(self):
         if self.contain_uni:
             new_result_list = []
             for result in self.result_list:
-                if self._contain_(result[0]):
+                if self._contain_(result):
                     new_result_list.append(result)
             self.result_list = new_result_list
 
     @log_wrap
     def _contain_(self, result):
-        return True if self.contain_uni in result else False
+        return True if self.contain_uni in result[0] else False
 
     def _size_limits_(self):
         if self.size_min or self.size_max:
-            new_result_list = []
-            for result in self.result_list:
-                if self._size_limit_(int(result[1].replace(',', ''))):
-                    new_result_list.append(result)
-            self.result_list = new_result_list
+            self.result_list = [result for result in self.result_list if self._size_limit_(result)]
 
     @log_wrap
     def _size_limit_(self, result):
-        return True if self.size_min <= result <= self.size_max else False
+        return True if self.size_min <= int(result[1].replace(',', '')) <= self.size_max else False
